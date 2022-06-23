082a58ec4e03b4a680eabe770dd57902cbba2f69
Kruchyxe <krzysiekmilczanowski@interia.eu>
2022-06-23 22:25:08 +0200

    2nd commit adding validation to User class

--- src/main/java/pl/kruchyxe/elisaac/model/User.java
+++ src/main/java/pl/kruchyxe/elisaac/model/User.java
@@ -1,25 +1,39 @@
 package pl.kruchyxe.elisaac.model;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.UniqueElements;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Data
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    private String name;
+    @NotBlank
+    @Size(min = 1, max = 50)
+    @Column(name="first_name")
+    private String firstName;
 
-    private String Surname;
+    @NotBlank
+    @Size(min = 1, max = 50)
+    @Column(name="last_name")
+    private String lastName;
 
+    @Email
+    @NotBlank
     private String email;
 
+    @NotBlank
     private String password;
 
 }
