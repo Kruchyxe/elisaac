e90d96f29f4cdf9ade34c05565f9d0ff0cc4972d
Kruchyxe <krzysiekmilczanowski@interia.eu>
2022-06-23 21:57:58 +0200

    Initial commit

---
+++ src/main/java/pl/kruchyxe/elisaac/ElisaacApplication.java
@@ -0,0 +1,13 @@
+package pl.kruchyxe.elisaac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElisaacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElisaacApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kruchyxe/elisaac/ElisaacApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kruchyxe.elisaac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElisaacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
